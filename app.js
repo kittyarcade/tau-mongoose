7409560052deb445d7c5b0e8128c808d987b3447
Millie Walsh <millie@primeacademy.io>
2017-01-05 11:00:39 -0600

    show url params and query params examples

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ app.use(bodyParser.json());
 //router
 app.use('/user', userRouter);
 
+app.get('/test/:id?', function(req, res) {
+  console.log( 'in test get route ');
+  console.log('req.params -> ', req.params.id);
+  console.log('req.query ->', req.query);
+  res.sendStatus(200);
+});
+
+// http://localhost:3000/test?username=millie - query string
+// http://localhost:3000/test/9192837 - url params
+
 //tauUsersDatabase is the name of the database
 mongoose.connect('mongodb://localhost:27017/tauUsersDatabase');
 
